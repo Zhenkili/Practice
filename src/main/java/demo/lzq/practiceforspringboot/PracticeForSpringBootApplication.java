2e14d7e6fe136becd60f0cea3f6818886b5d14f2
Zhenqi <zhenqi024@gmail.com>
2018-07-12 17:44:54 +0800

    first try

---
+++ src/main/java/demo/lzq/practiceforspringboot/PracticeForSpringBootApplication.java
@@ -0,0 +1,28 @@
+package demo.lzq.practiceforspringboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class PracticeForSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeForSpringBootApplication.class, args);
+    }
+
+    @RequestMapping(value="/",produces = "text/plain;charset=UTF-8")
+    String Index(){
+        return "hello I wanna read " + bookname +" which is wriiten by" + bookauthor +"and the part is "+ booksubname;
+    }
+
+    @Value(value="${book.name}")
+    private String bookname;
+    @Value(value="${book.author}")
+    private String bookauthor;
+    @Value(value="${book.subname}")
+    private String booksubname;
+}
